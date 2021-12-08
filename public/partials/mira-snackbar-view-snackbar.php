43973034448307c8b26b12439f0255af454de0b6
Delave-las-Kure <t1evgeny93@gmail.com>
2021-12-08 21:11:27 +0300

    up

--- public/partials/mira-snackbar-view-snackbar.php
+++ public/partials/mira-snackbar-view-snackbar.php
@@ -12,7 +12,6 @@
  * @subpackage Mira_Snackbar/public/partials
  * @var $snackbar_data
  */
-
 $list_class = apply_filters('mira_snackbar_el_class', [
     $snackbar_data['vertical_position'],
     $snackbar_data['horizontal_position'],
@@ -27,49 +26,61 @@ $list_class = apply_filters('mira_snackbar_el_class', [
     ?>
     <div class="mira-snackbar-list<?= $list_class ? ' ' . $list_class : '' ?>">
         <? foreach ($snackbar_data['snackbars'] as $snackbar) {
-     
-            $el_style = apply_filters('mira_snackbar_el_style', [
-                'background-color' => $snackbar['background_color'],
-                'color' => $snackbar['text_color']
+            $q = new WP_Query([
+                'numberposts'      => 1,
+                'post_type'        => 'mira_snackbar',
+                'post__in' => array($snackbar['id'])
             ]);
-            $el_class = apply_filters('mira_snackbar_el_class', [
-                'align-' . $snackbar['align_content']
-            ], 'mira-snackbar--');
+            if ($q->have_posts()) {
+                while ($q->have_posts()) {
+                    $q->the_post();
+                    $el_style = apply_filters('mira_snackbar_el_style', [
+                        'background-color' => $snackbar['background_color'],
+                        'color' => $snackbar['text_color']
+                    ]);
+                    $el_class = apply_filters('mira_snackbar_el_class', [
+                        'align-' . $snackbar['align_content'],
+                        $snackbar['show_delay'] ? 'delayed' : ''
+                    ], 'mira-snackbar--');
         ?>
-            <div class="mira-snackbar-container">
-                <div class="mira-snackbar<?= $el_class ? ' ' . $el_class : '' ?>" data-sid="<?= $snackbar['id'] ?>" data-show-after="<?= $snackbar['show_after'] ?>" <? if ($snackbar['background_color'] || $snackbar['text_color']) { ?> style="<?= $el_style ?>" <? } ?>>
-                    <div class="mira-snackbar__inner">
-                        <div class="mira-snackbar__content">
-                            <?= apply_filters('the_content', $snackbar['content']) ?>
+                    <div class="mira-snackbar-container">
+                        <div class="mira-snackbar<?= $el_class ? ' ' . $el_class : '' ?>" data-sid="<?= $snackbar['id'] ?>" data-delay="<?= $snackbar['show_delay'] ?>" data-show-after="<?= $snackbar['show_after'] ?>" <? if ($snackbar['background_color'] || $snackbar['text_color']) { ?> style="<?= $el_style ?>" <? } ?>>
+                            <div class="mira-snackbar__inner">
+                                <div class="mira-snackbar__content">
+                                    <? the_content() ?>
+                                </div>
+                                <? if ($snackbar['show_action_button']) { ?>
+                                    <div class="mira-snackbar__actions">
+                                        <?= apply_filters('mira_snackbar_btn', $snackbar['action_button']) ?>
+                                    </div>
+                                <? } ?>
+                            </div>
+                            <? if (!$snackbar['hide_close_button'] && (!$snackbar['action_button'] || $snackbar['action_button']['action'] !== 'close')) { ?>
+                                <div role="button" class="mira-snackbar__close" title="close">
+                                    <i class="dashicons dashicons-no-alt"></i>
+                                </div>
+                            <? } ?>
                         </div>
-                        <? if ($snackbar['show_action_button']) { ?>
-                            <div class="mira-snackbar__actions">
-                                <?= apply_filters('mira_snackbar_btn', $snackbar['action_button']) ?>
+                        <? if ($snackbar['show_action_button'] && $snackbar['action_button']['action'] == 'popup') {
+                            $inner_popup_style = apply_filters('mira_snackbar_el_style', [
+                                'max-width' => $snackbar['popup_width'] . 'px',
+                            ])
+                        ?>
+                            <div class="mira-snackbar-popup" style="display: none">
+                                <div class="mira-snackbar-popup__inner" <? if ($inner_popup_style) { ?> style="<?= $inner_popup_style ?>" <? } ?>>
+                                    <div class="mira-snackbar-popup__content">
+                                        <?= do_shortcode($snackbar['popup_text']) ?>
+                                    </div>
+                                    <div role="button" class="mira-snackbar-popup__close" title="close">
+                                    </div>
+                                </div>
                             </div>
                         <? } ?>
                     </div>
-                    <? if (!$snackbar['hide_close_button'] && (!$snackbar['action_button'] || $snackbar['action_button']['action'] !== 'close')) { ?>
-                        <div role="button" class="mira-snackbar__close" title="close">
-                            <i class="dashicons dashicons-no-alt"></i>
-                        </div>
-                    <? } ?>
-                </div>
-                <? if ($snackbar['show_action_button'] && $snackbar['action_button']['action'] == 'popup') {
-                    $inner_popup_style = apply_filters('mira_snackbar_el_style', [
-                        'max-width' => $snackbar['popup_width'] . 'px',
-                    ])
-                ?>
-                    <div class="mira-snackbar-popup" style="display: none">
-                        <div class="mira-snackbar-popup__inner" <? if ($inner_popup_style) { ?> style="<?= $inner_popup_style ?>" <? } ?>>
-                            <div class="mira-snackbar-popup__content">
-                                <?= apply_filters('the_content', $snackbar['popup_text']) ?>
-                            </div>
-                            <div role="button" class="mira-snackbar-popup__close" title="close">
-                            </div>
-                        </div>
-                    </div>
-                <? } ?>
-            </div>
-        <? } ?>
+        <?
+                    wp_reset_postdata();
+                }
+            }
+        } ?>
     </div>
 <? } ?>
