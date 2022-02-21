6ee2f4981325f0a84defd004ab0070e82e6ebaa4
Delave-las-Kure <t1evgeny93@gmail.com>
2022-02-22 02:35:22 +0300

    up

--- includes/class-mira-snackbar.php
+++ includes/class-mira-snackbar.php
@@ -70,7 +70,7 @@ class Mira_Snackbar {
 		if ( defined( 'MIRA_SNACKBAR_VERSION' ) ) {
 			$this->version = MIRA_SNACKBAR_VERSION;
 		} else {
-			$this->version = '1.0.0';
+			$this->version = '1.0.5';
 		}
 		$this->plugin_name = 'mira-snackbar';
 
